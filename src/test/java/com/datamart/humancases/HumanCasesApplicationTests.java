748c7b3d90ab235f28074b9f33eebb3bb75c62c4
BhrunoB8 <bhruno.roan@yahoo.com>
2022-07-12 14:44:37 -0300

    Testando a integração continua

--- src/test/java/com/datamart/humancases/HumanCasesApplicationTests.java
+++ src/test/java/com/datamart/humancases/HumanCasesApplicationTests.java
@@ -2,12 +2,14 @@ package com.datamart.humancases;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class HumanCasesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(true, "Test passing");
 	}
 
 }
